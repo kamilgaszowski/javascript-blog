e76d8e7750c09f2f2a6e079b64ab0d0e8dfe27f4
Kamil Gąszowski <kamil.gaszowski@gmail.com>
2019-03-28 23:06:49 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +12,14 @@ function titleClickHandler(event){
   	activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS]add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('article.active');
 
